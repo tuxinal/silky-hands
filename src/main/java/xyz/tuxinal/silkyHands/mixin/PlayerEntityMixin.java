d9dc13ec7b3b72e1d215cd1357e76b6795f97062
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2021-11-02 09:17:48 +0330

    Fix crash

--- src/main/java/xyz/tuxinal/silkyHands/mixin/PlayerEntityMixin.java
+++ src/main/java/xyz/tuxinal/silkyHands/mixin/PlayerEntityMixin.java
@@ -4,26 +4,26 @@ import java.util.Arrays;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.registry.Registry;
 import xyz.tuxinal.silkyHands.utils.ConfigParser;
 
-@Mixin(ServerPlayerInteractionManager.class)
-public class InteractionManagerMixin {
-    @Redirect(method = "tryBreakBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;canHarvest(Lnet/minecraft/block/BlockState;)Z"))
-    private boolean redirect(ServerPlayerEntity player, BlockState blockState) {
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @Inject(method = "canHarvest(Lnet/minecraft/block/BlockState;)Z", at = @At("RETURN"), cancellable = true)
+    private void injected(BlockState blockState, CallbackInfoReturnable<Boolean> ci) {
+        PlayerEntity player = ((PlayerEntity) (Object) this);
         if (player.getScoreboardTags().contains(ConfigParser.getTag())) {
             if (player.getMainHandStack().isEmpty()) {
                 if (!Arrays.stream(ConfigParser.getIgnoredBlocks())
                         .anyMatch(Registry.BLOCK.getId(blockState.getBlock()).toString()::equals)) {
-                    return true;
+                    ci.setReturnValue(true);
                 }
             }
         }
-        return player.canHarvest(blockState);
     }
 }
