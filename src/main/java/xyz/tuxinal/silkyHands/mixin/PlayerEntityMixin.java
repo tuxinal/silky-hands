7696f481454948a042195ecef5795a6612cb0ff9
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2021-11-30 13:14:22 +0330

    slight refactoring

--- src/main/java/xyz/tuxinal/silkyHands/mixin/PlayerEntityMixin.java
+++ src/main/java/xyz/tuxinal/silkyHands/mixin/PlayerEntityMixin.java
@@ -1,7 +1,6 @@
 package xyz.tuxinal.silkyHands.mixin;
 
-import java.util.Arrays;
-
+import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,13 +16,21 @@ public class PlayerEntityMixin {
     @Inject(method = "canHarvest(Lnet/minecraft/block/BlockState;)Z", at = @At("RETURN"), cancellable = true)
     private void injected(BlockState blockState, CallbackInfoReturnable<Boolean> ci) {
         PlayerEntity player = ((PlayerEntity) (Object) this);
-        if (player.getScoreboardTags().contains(ConfigParser.getTag())) {
-            if (player.getMainHandStack().isEmpty()) {
-                if (!Arrays.stream(ConfigParser.getIgnoredBlocks())
-                        .anyMatch(Registry.BLOCK.getId(blockState.getBlock()).toString()::equals)) {
-                    ci.setReturnValue(true);
-                }
-            }
+        // make sure the player has the required tag
+        if (!player.getScoreboardTags().contains(ConfigParser.getTag())) {
+            return;
         }
+        if (!player.getMainHandStack().isEmpty()) {
+            return;
+        }
+        // make sure the broken block is not ignored
+        // this check gets ran twice (once when checking harvestability and once when
+        // breaking block (BlockMixin))
+        // ideally this should only be checked once
+        if (ArrayUtils.contains(ConfigParser.getIgnoredBlocks(),
+                Registry.BLOCK.getId(blockState.getBlock()).toString())) {
+            return;
+        }
+        ci.setReturnValue(true);
     }
 }
