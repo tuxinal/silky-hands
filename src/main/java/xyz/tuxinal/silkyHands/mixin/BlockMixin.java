e53241cbc78a4daf10bfb6bf87b83ef74f8d90ab
Tuxinal <tuxinal@protonmail.com>
2023-07-04 17:34:15 +0330

    Overhaul silk touch-ing mechanisms many of the previously ignored blocks are not required to be ignored anymore oh wait how old is 1.19 again

--- src/main/java/xyz/tuxinal/silkyHands/mixin/BlockMixin.java
+++ src/main/java/xyz/tuxinal/silkyHands/mixin/BlockMixin.java
@@ -1,51 +1,63 @@
 package xyz.tuxinal.silkyHands.mixin;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.predicates.LootItemConditions;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
-import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerLevel;
 import xyz.tuxinal.silkyHands.utils.ConfigParser;
 
 @Mixin(Block.class)
 public class BlockMixin {
-    // @formatter:off 
-    @Inject(
-        method = "dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V",
-        at = @At(
-            value = "INVOKE", 
-            target = "Lnet/minecraft/world/level/block/Block;getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;"
-        ),
-        cancellable = true
-    )
-    // @formatter:on
-    private static void injected(BlockState blockState, Level world, BlockPos blockPos, BlockEntity blockEntity,
-            Entity entity, ItemStack stack, CallbackInfo ci) {
+    @Redirect(method = "dropResources(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/entity/BlockEntity;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/item/ItemStack;)Ljava/util/List;"))
+    private static List<ItemStack> inject(BlockState blockState, ServerLevel serverLevel,
+            BlockPos blockPos,
+            @Nullable BlockEntity blockEntity, @Nullable Entity entity, ItemStack itemStack) {
+        var originalDrops = Block.getDrops(blockState, serverLevel, blockPos, blockEntity, entity,
+                itemStack);
         if (!(entity instanceof Player)) {
-            return;
+            return originalDrops;
         }
         Player player = (Player) entity;
         if (!player.getTags().contains(ConfigParser.getTag())) {
-            return;
+            return originalDrops;
         }
         if (!player.getMainHandItem().isEmpty()) {
-            return;
+            return originalDrops;
         }
         if (ArrayUtils.contains(ConfigParser.getIgnoredBlocks(),
                 Registry.BLOCK.getKey(blockState.getBlock()).toString())) {
-            return;
+            return originalDrops;
         }
-        Block.popResource(world, blockPos, new ItemStack(blockState.getBlock()));
-        ci.cancel();
+        if (originalDrops.stream().anyMatch(stack -> stack.getItem() == blockState.getBlock().asItem())) {
+            return originalDrops;
+        }
+
+        // really hacky solution to multiblocks having dropping multiple times when
+        // broken
+        // as far as i can tell all vanilla multiblocks work fine with this
+        var block = blockState.getBlock();
+        var lootContext = block.getLootTable();
+        var lootTable = serverLevel.getServer().getLootTables().get(lootContext);
+        if (Stream.of(lootTable.pools[0].entries[0].conditions)
+                .anyMatch(condition -> condition.getType() == LootItemConditions.BLOCK_STATE_PROPERTY)) {
+            return originalDrops;
+        }
+
+        return List.of(new ItemStack(blockState.getBlock()));
     }
 }
--- src/main/java/xyz/tuxinal/silkyHands/mixin/PlayerEntityMixin.java
+++ src/main/java/xyz/tuxinal/silkyHands/mixin/PlayerEntityMixin.java
@@ -16,17 +16,12 @@ public class PlayerEntityMixin {
     @Inject(method = "hasCorrectToolForDrops(Lnet/minecraft/world/level/block/state/BlockState;)Z", at = @At("RETURN"), cancellable = true)
     private void injected(BlockState blockState, CallbackInfoReturnable<Boolean> ci) {
         Player player = ((Player) (Object) this);
-        // make sure the player has the required tag
         if (!player.getTags().contains(ConfigParser.getTag())) {
             return;
         }
         if (!player.getMainHandItem().isEmpty()) {
             return;
         }
-        // make sure the broken block is not ignored
-        // this check gets ran twice (once when checking harvestability and once when
-        // breaking block (BlockMixin))
-        // ideally this should only be checked once
         if (ArrayUtils.contains(ConfigParser.getIgnoredBlocks(),
                 Registry.BLOCK.getKey(blockState.getBlock()).toString())) {
             return;
