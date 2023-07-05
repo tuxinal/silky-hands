91f29f0bf7fe40288be34b5c8cb21baf288f286f
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2023-07-05 13:18:59 +0330

    Overhaul silk touch-ing mechanisms many of the previously ignored blocks are not required to be ignored anymore oh wait how old is 1.19 again

--- src/main/java/xyz/tuxinal/silkyHands/utils/ConfigParser.java
+++ src/main/java/xyz/tuxinal/silkyHands/utils/ConfigParser.java
@@ -21,7 +21,6 @@ public class ConfigParser {
     private static Logger LOGGER = LogManager.getLogger();
 
     public static void init() {
-
         GsonBuilder builder = new GsonBuilder();
         builder.setPrettyPrinting();
         Gson gson = builder.create();
@@ -31,30 +30,7 @@ public class ConfigParser {
                 configFile.createNewFile();
                 FileWriter writer = new FileWriter(configFile);
                 Config configTemplate = new Config();
-                configTemplate.ignoredBlocks = new String[] { "minecraft:white_bed", "minecraft:orange_bed",
-                        "minecraft:magenta_bed", "minecraft:light_blue_bed", "minecraft:yellow_bed",
-                        "minecraft:lime_bed", "minecraft:pink_bed", "minecraft:gray_bed", "minecraft:light_gray_bed",
-                        "minecraft:cyan_bed", "minecraft:purple_bed", "minecraft:blue_bed", "minecraft:brown_bed",
-                        "minecraft:green_bed", "minecraft:red_bed", "minecraft:black_bed", "minecraft:beetroots",
-                        "minecraft:wheat", "minecraft:potatoes", "minecraft:carrots",
-                        "minecraft:shulker_box",
-                        "minecraft:white_shulker_box",
-                        "minecraft:orange_shulker_box",
-                        "minecraft:magenta_shulker_box",
-                        "minecraft:light_blue_shulker_box",
-                        "minecraft:yellow_shulker_box",
-                        "minecraft:lime_shulker_box",
-                        "minecraft:pink_shulker_box",
-                        "minecraft:gray_shulker_box",
-                        "minecraft:light_gray_shulker_box",
-                        "minecraft:cyan_shulker_box",
-                        "minecraft:purple_shulker_box",
-                        "minecraft:blue_shulker_box",
-                        "minecraft:brown_shulker_box",
-                        "minecraft:green_shulker_box",
-                        "minecraft:red_shulker_box",
-                        "minecraft:black_shulker_box",
-                };
+                configTemplate.ignoredBlocks = new String[] {};
                 configTemplate.tag = "silkyhands";
                 writer.write(gson.toJson(configTemplate));
                 writer.close();
